b167f19f0505b28e752d0c12a43d56d4438fb84a
Vansh Mahajan <vanshmahajan2023@gmail.com>
2025-12-27 21:50:46 +0530

    Auth Middleware jwt

--- auth.go
+++ auth.go
@@ -2,11 +2,65 @@ package main
 
 import (
 	"net/http"
-
+	"strings"
+	"time"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var users = map[string]string{}
+var jwtSecret = []byte("supersecretkey")
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		authHeader := ctx.GetHeader("Authorization")
+
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			ctx.JSON(401, gin.H{"error": "missing or invalid token"})
+			ctx.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method")
+			}
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			ctx.JSON(401, gin.H{"error": "invalid token"})
+			ctx.Abort()
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			ctx.JSON(401, gin.H{
+				"error": "Could not read claims",
+			})
+			ctx.Abort()
+			return 
+		}
+
+		email, ok := claims["email"].(string)
+	if !ok {
+		ctx.JSON(401, gin.H{"error": "invalid token payload"})
+		ctx.Abort()
+		return
+	}
+
+		ctx.Set("email",email)
+		ctx.Next()
+	}
+}
+
+
 
 func LoginHandler(ctx *gin.Context) {
 	var body struct {
@@ -14,25 +68,34 @@ func LoginHandler(ctx *gin.Context) {
 		Password string `json:"password" binding:"required"`
 	}
 
-	if err := ctx.ShouldBindJSON(&body); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": "email and password are required",
-		})
+	storedPassword, exists := users[body.Email]
+	if !exists {
+		ctx.JSON(401, gin.H{"error": "invalid email or password"})
 		return
 	}
 
-	storedPassword, exists := users[body.Email]
+	if err := bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(body.Password)); err != nil {
+		ctx.JSON(401, gin.H{"error": "invalid email or password"})
+		return
+	}
 
-	if !exists || storedPassword != body.Password {
-		ctx.JSON(401, gin.H{
-			"error": "invalid email or password",
-		})
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": body.Email,
+		"exp":   time.Now().Add(24 * time.Hour).Unix(),
+	})
+
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": "could not create token"})
 		return
 	}
 
 	ctx.JSON(200, gin.H{
-		"message": "login successfull",
-	})
+		"message": "login successful",
+		"token":   tokenString,
+})
+
 }
 
 func SignupHandler(ctx *gin.Context) {
@@ -55,9 +118,31 @@ func SignupHandler(ctx *gin.Context) {
 		return
 	}
 
-	users[body.Email] = body.Password
+
+	hashed , err := bcrypt.GenerateFromPassword([]byte(body.Password),10)
+	if err != nil {
+		ctx.JSON(500,gin.H{
+			"error": "Could not create User",
+		})
+		return
+	}
+
+	users[body.Email] = string(hashed)
+
+
 
 	ctx.JSON(200, gin.H{
 		"message": "user created successfully",
+	})
+
+}
+
+
+func protectedRoute(ctx *gin.Context){
+	
+	email,_ := ctx.Get("email")
+
+	ctx.JSON(200,gin.H{
+		"user" : email,
 	})
 }
--- main.go
+++ main.go
@@ -8,20 +8,16 @@ func main() {
 	r := gin.Default()
 	r.Use(Logger()) //we use "Use" attches global middleware to router
 
-	// Basic routes
+
 	r.GET("/welcome", WelcomeHandler)
 	r.GET("/books/:id", BooksHandler)
 	r.GET("/add", AddHandler)
-
-	// User routes
 	api := r.Group("/user")
 	api.GET("/profile", CheckHeader(), ProfileHandler)
-
 	r.GET("/dashboard", CheckHeader(), DashboardHandler)
-
-	// Auth routes
 	r.POST("/login", LoginHandler)
 	r.POST("/signup", SignupHandler)
+	r.GET("/me", AuthMiddleware(),protectedRoute)
 
 	r.Run(":8080")
 }
